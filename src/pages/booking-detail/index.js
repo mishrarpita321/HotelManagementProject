6bfd587fa165cb78e3909d072dd9742b7f74c1ee
mishrarpita321 <45449505+mishrarpita321@users.noreply.github.com>
2023-05-30 15:45:22 +0200

    css changes

--- src/pages/booking-detail/index.js
+++ src/pages/booking-detail/index.js
@@ -23,12 +23,12 @@ export default function BookingDetail() {
                         <div className="row">
                             <div className="col-md-6">
                                 <div className="row chekin-checkout">
-                                    <div className="col-md-3">
+                                    <div className="col">
                                         <div className="row">CHECK IN</div>
                                         <div className="row">SUN 4JUN 23</div>
                                         <div className="row">10:00</div>
                                     </div>
-                                    <div className="col-md-3" style={{ margin: "0 0 0 150px" }}>
+                                    <div className="col" style={{ margin: "0 0 0 150px" }}>
                                         <div className="row">CHECK OUT</div>
                                         <div className="row">SUN 5JUN 23</div>
                                         <div className="row">10:00</div>
@@ -42,12 +42,12 @@ export default function BookingDetail() {
                         <div className="row liItems">
                             <div className="col">
                                 <h5>Important Details</h5>
-                                <ul>
+                                <ol>
                                     <li>Room With Breakfast + Lunch + Dinner</li>
                                     <li>Passport, Aadhar, Driving License and Govt. ID are accepted as ID proof(s)</li>
                                     <li>Pets are allowed.</li>
                                     <li>Outside food is not allowed.</li>
-                                </ul>
+                                </ol>
                             </div>
                         </div>
                         <div className="row guestForm" >
@@ -151,67 +151,45 @@ export default function BookingDetail() {
                             <div className="row">
                                 <h5>Price Breakup</h5>
                             </div>
-                            <div className="row priceRow" >
-                                <div className="col">
-                                    <div className="row">
-                                        <p>1 Room x 1 Night</p>
+                            <div className="container" style={{marginTop: "14px"}}>
+                                <div className="row priceRow" >
+                                    <div className="col">
+                                        <div className="row">
+                                            <p>1 Room x 1 Night</p>
+                                        </div>
+                                    </div>
+                                    <div className="col">
+                                        <div className="row priceAlign">
+                                            <p>$ 21</p>
+                                        </div>
                                     </div>
                                 </div>
-                                <div className="col">
-                                    <div className="row priceAlign">
-                                        <p>$ 21</p>
+                                <div className="row priceRow">
+                                    <div className="col">
+                                        <div className="row">
+                                            <p>Service Fees</p>
+                                        </div>
+                                    </div>
+                                    <div className="col">
+                                        <div className="row priceAlign">
+                                            <p>$ 10</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="row" style={{ backgroundColor: "burlywood" }}>
+                                    <div className="col">
+                                        <div className="row">
+                                            <p>Total Amount to be paid</p>
+                                        </div>
+                                    </div>
+                                    <div className="col">
+                                        <div className="row priceAlign">
+                                            <p>$ 210</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                            <div className="row priceRow">
-                                <div className="col">
-                                    <div className="row">
-                                        <p>Service Fees</p>
-                                    </div>
-                                </div>
-                                <div className="col">
-                                    <div className="row priceAlign">
-                                        <p>$ 10</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="row" style={{ backgroundColor: "burlywood"}}>
-                                <div className="col">
-                                    <div className="row">
-                                        <p>Total Amount to be paid</p>
-                                    </div>
-                                </div>
-                                <div className="col">
-                                    <div className="row priceAlign">
-                                        <p>$ 210</p>
-                                    </div>
-                                </div>
-                            </div>
 
-                            {/* <div className="card">
-                                    <div class="card-body">
-                                        <h5>Price Breakup</h5>
-                                        <p>1 Room x 1 Night</p>
-
-                                        <h6 class="card-subtitle mb-2 text-muted">Base Price</h6>
-                                        <p class="card-text">₹ 21,000</p>
-
-                                        <h6 class="card-subtitle mb-2 text-muted">Total Discount</h6>
-                                        <p class="card-text">₹ 1,470</p>
-
-                                        <h6 class="card-subtitle mb-2 text-muted">Price after Discount</h6>
-                                        <p class="card-text">₹ 19,530</p>
-
-                                        <h6 class="card-subtitle mb-2 text-muted">Service Fees</h6>
-                                        <p class="card-text">₹ 1,470</p>
-
-                                        <h6 class="card-subtitle mb-2 text-muted">Donate ₹5 to support responsible tourism initiatives. T&amp;Cs</h6>
-                                        <p class="card-text">₹ 5</p>
-
-                                        <h6 class="card-subtitle mb-2 text-muted">Total Amount to be paid</h6>
-                                        <p class="card-text">₹ 21,005</p>
-                                    </div>
-                                </div> */}
                         </div>
                     </div>
                 </div>
