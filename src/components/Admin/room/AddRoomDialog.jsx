a1f0332e9aa5f588c1309dc277a6cc17d0c9574e
Roshan <devkkali@gmail.com>
2023-06-11 16:13:07 +0200

    room completed

---
+++ src/components/Admin/room/AddRoomDialog.jsx
@@ -0,0 +1,291 @@
+import { useState, useEffect, useContext } from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { Form, Button, Container, Row, Col, Spinner } from 'react-bootstrap';
+import { AlertContext } from 'src/context/AlertContext';
+import { useDispatch } from 'react-redux';
+import { addAdminRooms } from 'src/store/admin/rooms';
+
+const schema = yup.object().shape({
+    roomNo: yup
+        .number()
+        .typeError('Room No must be a number')
+        .positive('Room No must be a positive number')
+        .integer('Room No must be an integer')
+        .required('Room No is required'),
+    category: yup.string().required('Category is required'),
+    roomStatus: yup.boolean().required('Status is required'),
+    inventory: yup.boolean().required('Inventory availability is required'),
+    cleaningStatus: yup.boolean().required('Cleaning status is required'),
+    image: yup.mixed().required('Please select an image'),
+});
+
+const defaultValues = {
+    roomNo: '',
+    category: '',
+    roomStatus: false,
+    inventory: false,
+    cleaningStatus: false,
+    image: null,
+};
+
+export default function AddRoomDialog({ setShowAddDialog, categoryStore }) {
+    const { showAlert } = useContext(AlertContext);
+    const dispatch = useDispatch();
+    const [isLoading, setIsLoading] = useState(false);
+    const [categories, setCategories] = useState([]);
+    const {
+        handleSubmit,
+        control,
+        formState: { errors },
+        setValue,
+    } = useForm({
+        defaultValues,
+        resolver: yupResolver(schema),
+    });
+
+    useEffect(() => {
+        setCategories(categoryStore.data);
+    }, [categoryStore]);
+
+    // const onSubmit = (data) => {
+    //     setIsLoading(true);
+
+    //     const payload = {
+    //         roomNo: data.roomNo,
+    //         isActive: data.roomStatus === true,
+    //         isInventoryAvailable: data.inventory,
+    //         isCleaned: data.cleaningStatus,
+    //         categoryId: data.category,
+    //         image: data.image[0], // Assuming only one image is selected
+    //     };
+    //     console.log(data)
+    //     console.log(payload)
+    //     // Perform your form submission logic here
+
+    //     setIsLoading(false);
+    // };
+
+    const onSubmit = (data) => {
+        setIsLoading(true)
+
+
+
+        // const payload = {
+        //     roomNo: data.roomNo,
+        //     isActive: data.roomStatus === true,
+        //     isInventoryAvailable: data.inventory,
+        //     isCleaned: data.cleaningStatus,
+        //     categoryId: data.category,
+        //     image: data.image[0], // Assuming only one image is selected
+        // };
+        // console.log(data)
+        // console.log(payload)
+
+
+
+        // const { name, rooms, size, capacity, price, image } = data;
+        const roomDto = {
+            roomNo: data.roomNo,
+            isActive: data.roomStatus === true,
+            isInventoryAvailable: data.inventory,
+            isCleaned: data.cleaningStatus,
+        };
+        const test = JSON.stringify(roomDto)
+
+
+        const blob = new Blob([test], {
+            type: 'application/json'
+        })
+
+        console.log(data.image)
+        const formData = new FormData();
+        formData.append('categoryId', data.category);
+        formData.append('image', data.image);
+        formData.append('roomDto', blob);
+
+        dispatch(addAdminRooms(formData)).then((data) => {
+            // console.log(data)
+            if (data?.payload?.status === 'success') {
+                onAlertSuccessHandle(data?.payload?.message);
+            } else {
+                onAlertErrorHandle(data?.payload?.message);
+            }
+            setIsLoading(false)
+        });
+    };
+
+    const onAlertErrorHandle = (message) => {
+        showAlert('error', message, () => {
+            console.log('Ok button clicked');
+        });
+    };
+
+    const onAlertSuccessHandle = (message) => {
+        showAlert('success', message, () => { }, () => { }, () => {
+            setShowAddDialog(false)
+        });
+    };
+
+
+
+
+
+    return (
+        <Container>
+            <h5 className="text-center mt-3">Add Room</h5>
+            <Form onSubmit={handleSubmit(onSubmit)}>
+                <Row className="mb-3">
+                    <Col>
+                        <Form.Group controlId="roomNo">
+                            <Form.Label>Room No:</Form.Label>
+                            <Controller
+                                control={control}
+                                name="roomNo"
+                                render={({ field }) => (
+                                    <Form.Control
+                                        type="text"
+                                        {...field}
+                                        disabled={isLoading}
+                                        isInvalid={errors.roomNo}
+                                    />
+                                )}
+                            />
+                            {errors.roomNo && (
+                                <Form.Text className="text-danger">{errors.roomNo.message}</Form.Text>
+                            )}
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Form.Group controlId="category">
+                            <Form.Label>Category:</Form.Label>
+                            <Controller
+                                control={control}
+                                name="category"
+                                render={({ field }) => (
+                                    <Form.Control as="select" {...field} disabled={isLoading} isInvalid={errors.category}>
+                                        <option value="">Select One</option>
+                                        {categories.map((category) => (
+                                            <option key={category.id} value={category.id}>
+                                                {category.title}
+                                            </option>
+                                        ))}
+                                    </Form.Control>
+                                )}
+                            />
+                            {errors.category && (
+                                <Form.Text className="text-danger">{errors.category.message}</Form.Text>
+                            )}
+                        </Form.Group>
+                    </Col>
+                </Row>
+
+                <Row className="mb-3">
+                    <Col>
+                        <Form.Group controlId="roomStatus">
+                            <Form.Label>Status:</Form.Label>
+                            <Controller
+                                control={control}
+                                name="roomStatus"
+                                render={({ field }) => (
+                                    <Form.Control
+                                        as="select"
+                                        {...field}
+                                        disabled={isLoading}
+                                        isInvalid={errors.roomStatus}
+                                    >
+                                        <option value={true}>Booked</option>
+                                        <option value={false}>Not Booked</option>
+                                    </Form.Control>
+                                )}
+                            />
+                            {errors.roomStatus && (
+                                <Form.Text className="text-danger">{errors.roomStatus.message}</Form.Text>
+                            )}
+                        </Form.Group>
+                    </Col>
+
+                    <Col>
+                        <Form.Group controlId="inventory">
+                            <Form.Label>Inventory Available?</Form.Label>
+                            <Controller
+                                control={control}
+                                name="inventory"
+                                render={({ field }) => (
+                                    <Form.Check
+                                        type="switch"
+                                        id="inventorySwitch"
+                                        label=""
+                                        {...field}
+                                        disabled={isLoading}
+                                    />
+                                )}
+                            />
+                        </Form.Group>
+                    </Col>
+
+                    <Col>
+                        <Form.Group controlId="cleaningStatus">
+                            <Form.Label>Cleaning Status?</Form.Label>
+                            <Controller
+                                control={control}
+                                name="cleaningStatus"
+                                render={({ field }) => (
+                                    <Form.Check
+                                        type="switch"
+                                        id="cleaningSwitch"
+                                        label=""
+                                        {...field}
+                                        disabled={isLoading}
+                                    />
+                                )}
+                            />
+                        </Form.Group>
+                    </Col>
+                </Row>
+
+                <Row className="mb-3">
+                    <Col>
+                        <Form.Group controlId="image">
+                            <Form.Label>Image:</Form.Label>
+                            <Controller
+                                control={control}
+                                name="image"
+                                render={({ field }) => (
+                                    <Form.Control
+                                        disabled={isLoading}
+                                        type="file"
+                                        accept="image/*"
+                                        onChange={(e) => {
+                                            const file = e.target.files[0];
+                                            setValue('image', file);
+                                        }}
+                                        isInvalid={errors.image}
+                                    />
+                                )}
+                            />
+                            {errors.image && (
+                                <Form.Text className="text-danger">{errors.image.message}</Form.Text>
+                            )}
+                        </Form.Group>
+                    </Col>
+                </Row>
+
+                <div className="text-center mt-3 mb-3">
+                    <Button disabled={isLoading} variant="primary" type="submit">
+                        {isLoading ? <Spinner animation="border" size="sm" /> : 'Submit'}
+                    </Button>
+                    <Button
+                        disabled={isLoading}
+                        variant="secondary"
+                        onClick={() => setShowAddDialog(false)}
+                        className="ml-2"
+                    >
+                        {isLoading ? <Spinner animation="border" size="sm" /> : 'Cancel'}
+                    </Button>
+                </div>
+            </Form>
+        </Container>
+    );
+}
--- src/pages/admin/room/index.jsx
+++ src/pages/admin/room/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 // import { Tab } from 'react-bootstrap';
 import Footer from 'src/components/Footer';
@@ -9,74 +9,98 @@ import TitleBanner from 'src/components/TitleBanner';
 import PropertyForm from "src/components/Admin/category/AddCategoryForm"
 import NoCloseModal from "src/components/modal/NoCloseModal"
 import SideBar4 from 'src/components/Admin/SideBar4';
-import AddRoomDialog from 'src/components/Admin/AddRoomDialog';
-
-
+import AddRoomDialog from 'src/components/Admin/room/AddRoomDialog';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteAdminRooms, fetchAdminRoomsList } from 'src/store/admin/rooms';
+import { fetchCategoriesList } from 'src/store/admin/category';
+import ImageModal from 'src/components/modal/ImageModal';
+import EditRoomDialog from 'src/components/Admin/room/EditRoomDialog';
+import { AlertContext } from 'src/context/AlertContext';
 const RoomList = () => {
-  const [showAddDialog, addRoomClick] = useState(false)
+  const [rooms, setRooms] = useState([])
+  const [showAddDialog, setShowAddDialog] = useState(false)
+  const [showEditDialog, setShowEditDialog] = useState(false)
+  const dispatch = useDispatch()
+  const categoryStore = useSelector(state => state.category)
+  const adminRoomStore = useSelector(state => state.adminRoom)
+  // const store = useSelector(state => state.category)
+  const { showAlert } = useContext(AlertContext);
 
 
 
   useEffect(() => {
-    console.log('$$$$$$$$$$$$$$$$$$$i am from useEffect');
-  }, []);
-
-
-
-
-
-  const router = useRouter();
-  const { query } = router;
-  const [rooms, setRooms] = useState([
-    { id: 1, roomNo: 122, status: true, inventory: true, cleaning: false, category: 'deluxe' },
-    { id: 2, roomNo: 222, status: true, inventory: true, cleaning: false, category: 'deluxe' },
-    { id: 3, roomNo: 322, status: true, inventory: true, cleaning: false, category: 'deluxe' },
-
-  ]);
-  console.log(rooms)
+    dispatch(fetchAdminRoomsList({}))
+    dispatch(fetchCategoriesList({}))
+  }, [dispatch])
 
 
   useEffect(() => {
-    fetchRooms();
-  }, [query.category]); // Fetch rooms whenever the category query parameter changes
+    setRooms(adminRoomStore.data)
+  }, [adminRoomStore])
 
 
-  // Function to fetch the list of rooms from the API based on the category
-  const fetchRooms = async () => {
-    try {
-      // Make API call with the appropriate endpoint and query parameters
-      const response = await fetch(`/api/rooms?category=${query.categoyry || ''}`);
-      const data = await response.json();
-      setRooms(data);
-    } catch (error) {
-      console.error('Error fetching rooms:', error);
+  console.log(adminRoomStore.data)
+
+  const getImageFormat = (data) => {
+    // Identify the image format based on the data
+    const mimeType = 'image/png'; // Default to PNG format if not recognized
+
+    // Extract the first few bytes of the data
+    const bytes = data.substring(0, 8);
+
+    // Check for known file signatures to identify the format
+    if (bytes.startsWith('data:image/jpeg') || bytes.startsWith('/9j/')) {
+      return 'image/jpeg';
     }
+    if (bytes.startsWith('data:image/png') || bytes.startsWith('iVBORw')) {
+      return 'image/png';
+    }
+    if (bytes.startsWith('data:image/gif') || bytes.startsWith('R0lGOD')) {
+      return 'image/gif';
+    }
+    // Add more format checks if necessary
+
+    return mimeType; // Return the default MIME type if the format is not recognized
   };
 
 
-  if (query.roomId) {
-    // Redirect to dynamic room details page if roomId is provided in the query
-    router.push(`/room/${query.roomId}`);
-    return null;
+  const [editRow, setEditRow] = useState(null);
+  const handelOnEditClicked = (room) => {
+    setEditRow(room)
+    setShowEditDialog(true)
+  }
+
+  const onAlertErrorHandle = (message) => {
+    showAlert('error', message, () => {
+      console.log('Ok button clicked');
+    });
+  };
+
+  const onAlertSuccessHandle = (message) => {
+    showAlert('success', message, () => { }, () => { }, () => {
+      setShowEditDialog(false)
+    });
+  };
+  const handleOnDeleteClicked = (room) => {
+    showAlert('confirmation', 'Are you sure to delete this Room?', () => {
+      dispatch(deleteAdminRooms(room?.id)).then((data) => {
+        if (data?.payload?.status === 'success') {
+          onAlertSuccessHandle(data?.payload?.message);
+        } else {
+          onAlertErrorHandle(data?.payload?.message);
+        }
+      })
+    }, () => {
+      console.log('Cancel is clicked')
+    });
+    // setEditRow(category)
+    // setShowEditDialog(true)
   }
 
 
-  // const addRoomClick = () => {
-  //   let newroom = { id: 4, roomNo: 322, status: true, inventory: true, cleaning: false, category: 'deluxe' }
-
-  //   setRooms([...rooms, newroom]);
-  // }
 
 
-  const deleteRoomClick = (id) => {
-    // alert(id)
-    let newroom = { id: 4, roomNo: 322, status: true, inventory: true, cleaning: false, category: 'deluxe' }
-    setRooms(rooms.filter((item) => item.id != newroom.id));
-
-  };
-
-
-
+  const [selectedImage, setSelectedImage] = useState(null);
   return (
     <>
       <NavBarAdmin />
@@ -88,11 +112,12 @@ const RoomList = () => {
         <div className='col-10'>
           <TitleBanner marginBotton={'40px'} padding={'7'} title={"Rooms"} />
           <div className="">
-            <button onClick={() => addRoomClick(true)} className="loginButton">+ Add Room</button>
+            <button onClick={() => setShowAddDialog(true)} className="loginButton">+ Add Room</button>
             <table className="table table-bordered table-hover transaction">
               <thead className="thead-dark">
                 <tr style={{ backgroundColor: "#38325059" }}>
                   <th scope="col">Room No</th>
+                  <th scope="col">Image</th>
                   <th scope="col">Category</th>
                   <th scope="col">Status</th>
                   <th scope="col">Inventory Available?</th>
@@ -101,28 +126,47 @@ const RoomList = () => {
                 </tr>
               </thead>
               <tbody>
-                {rooms.map((room, i) => {
-                  return (
-                    <tr key={i}>
-                      <th scope="row">{room.roomNo}</th>
-                      <td>{room.category}</td>
-                      <td>{(room.status ? (<>true</>) : (<>false</>))}</td>
-                      <td>{(room.inventory ? (<>true</>) : (<>false</>))}</td>
-                      <td>{(room.cleaning ? (<>true</>) : (<>false</>))}</td>
-                      <td>
-                        <button className="edit">Edit</button>
-                        <button onClick={deleteRoomClick.bind(this, 4)} className="delete">Delete</button>
-                      </td>
-                    </tr>
-                  )
+                {rooms.length !== 0 && (
+                  <>
+                    {rooms.map((room, i) => {
+                      console.log(room)
+                      return (
+                        <tr key={room?.id}>
+                          <th scope="row">{room?.roomNo}</th>
+                          <td>
+                            {/* <img src={`data:${getImageFormat(category?.imageData)};base64,${category?.imageData}`} alt="Category Image" style={{ height: "200px", width: "200px" }}></img> */}
+                            <img
+                              src={`data:${getImageFormat(room?.imageData)};base64,${room?.imageData}`}
+                              alt="Category Image"
+                              style={{ height: "200px", width: "200px", cursor: "pointer" }}
+                              onClick={() => setSelectedImage(`data:${getImageFormat(room?.imageData)};base64,${room?.imageData}`)}
+                            />
+                          </td>
+                          <td>{room?.category?.title}</td>
+                          <td>{(room?.isActive ? (<>Booked</>) : (<>Not Booked</>))}</td>
+                          <td>{(room?.isInventoryAvailable ? (<>Yes</>) : (<>No</>))}</td>
+                          <td>{(room?.isCleaned ? (<>Yes</>) : (<>No</>))}</td>
+                          <td>
+                            <button onClick={handelOnEditClicked.bind(this, room)} className="edit">Edit</button>
+                            <button onClick={handleOnDeleteClicked.bind(this, room)} className="delete">Delete</button>
 
-                })}
+                          </td>
+                        </tr>
+                      )
+
+                    })}
+                  </>
+                )}
               </tbody>
             </table>
           </div>
-          <NoCloseModal show={showAddDialog} onHide={() => { addRoomClick(false) }}>
+          <NoCloseModal show={showAddDialog} onHide={() => { setShowAddDialog(false) }}>
             {/* <AddCategoryDialog /> */}
-            <AddRoomDialog />
+            <AddRoomDialog setShowAddDialog={() => { setShowAddDialog(false) }} categoryStore={categoryStore} />
+          </NoCloseModal>
+          <ImageModal show={!!selectedImage} onHide={() => setSelectedImage(null)} image={selectedImage} />
+          <NoCloseModal show={showEditDialog} onHide={() => { setShowEditDialog(false) }}>
+            <EditRoomDialog setShowEditDialog={() => { setShowEditDialog(false) }} categoryStore={categoryStore} editRow={editRow} setEditRow={setEditRow} />
           </NoCloseModal>
         </div>
 
@@ -138,5 +182,8 @@ const RoomList = () => {
 
   );
 };
+RoomList.guestGuard = false
+RoomList.authGuard = true
+RoomList.adminGuard = true
 
 export default RoomList;
